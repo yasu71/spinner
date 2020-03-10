14ce35d99e93d0cc2d4e44a195e94d18e7ebf6f6
yasu71 <yasuyo71@gmail.com>
2020-03-10 23:29:49 +0000

    spinner2

---
+++ spinner2.js
@@ -0,0 +1,14 @@
+// pair programming of Jason@github.com/zeipar, Yasu@github.com/yasu71
+
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+const spinners = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ']
+let counter = 0;
+let time = 100;
+
+for (const spinner of spinners) {  
+  time += 200;
+  setTimeout(() => {
+    process.stdout.write(spinner);
+  }, time)
+}
